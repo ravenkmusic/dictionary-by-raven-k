5268e3cae8d24d4254ce6543ae14e52069feaa87
ravenkmusic <ravenkbeats@gmail.com>
2021-10-12 17:15:24 -0400

    installed axios and added API integration

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Search.css";
 
 export default function Search () {
     let [keyword, setKeyword] = useState("");
 
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
+
     function Define (event) {
         event.preventDefault();
         alert(`Searching for definition of ${keyword}...`);
+
+        let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiURL).then(handleResponse);
     }
 
     function handleKeywordChange (event) {
