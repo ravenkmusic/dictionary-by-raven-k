eb914059ef08c8ab6beeac19011439032393bdd6
ravenkmusic <ravenkbeats@gmail.com>
2021-10-12 18:47:17 -0400

    added more styling to page

---
+++ src/Results.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Results.css";
+
+export default function Results (props) {
+    return (<div className="Results">Testing...</div>);
+}
--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Search.css";
 
 export default function Search () {
@@ -7,6 +8,16 @@ export default function Search () {
 
     function handleResponse(response) {
         console.log(response.data[0]);
+        console.log(response.data[0].word);
+        console.log(response.data[0].phonetic);
+        console.log(response.data[0].meanings[0].partOfSpeech)
+        console.log(response.data[0].meanings[0].definitions[0].definition);
+        console.log(response.data[0].meanings[0].definitions[0].synonyms);
+        console.log(response.data[0].meanings[0].definitions[0].antonyms);
+        console.log(response.data[0].meanings[1].partOfSpeech)
+        console.log(response.data[0].meanings[1].definitions[0].definition);
+        console.log(response.data[0].meanings[1].definitions[0].synonyms);
+        console.log(response.data[0].meanings[1].definitions[0].antonyms);
     }
 
     function Define (event) {
@@ -26,6 +37,7 @@ export default function Search () {
             <form onSubmit={Define}>
                 <input type="search" autoFocus={true} onChange={handleKeywordChange}/>
             </form>
+            <Results />
         </div>
     );
 }
