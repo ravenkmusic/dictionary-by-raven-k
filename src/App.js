f1a38d45e9a78c228b47e222bddaa1a4c542bc1b
ravenkmusic <ravenkbeats@gmail.com>
2021-10-12 17:17:57 -0400

    added link to github

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1 className="display-2">A Dictionary by Raven K</h1>
       <Search />
-      <footer>Coded by <a href="https://ravenkmusic.com/" rel="noreferrer">Raven Hendricks</a></footer>
+      <footer><a href="https://github.com/ravenkmusic/dictionary-by-raven-k" target="_blank" rel="noreferrer">Coded</a> by <a href="https://ravenkmusic.com/" rel="noreferrer">Raven Hendricks</a></footer>
     </div>
   );
 }
